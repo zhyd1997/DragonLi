31b0eae39d5f3e4caf18c53887b6cd9f7ff8989f
Yadong Zhang <zhyd007@gmail.com>
2022-09-11 10:33:49 +0800

    feat: replaced css with css-in-js(emotion). (#2)
    + build: auto install peer deps.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import { globalStyles } from '../styles/globals'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {globalStyles}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ styles/globals.js
@@ -0,0 +1,9 @@
+import { Global, css } from "@emotion/react";
+
+export const globalStyles = (
+  <Global styles={css`
+    * {
+      box-sizing: border-box;
+    }
+  `}/>
+);
