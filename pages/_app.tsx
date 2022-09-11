d868d93a36dbb07e2b865be1a605dc287667d968
Yadong Zhang <zhyd007@gmail.com>
2022-09-11 14:31:06 +0800

    Revert "feat: replaced css with css-in-js(emotion). (#2)" (#3)
    This reverts commit 31b0eae39d5f3e4caf18c53887b6cd9f7ff8989f.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import { globalStyles } from '../styles/globals'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      {globalStyles}
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
