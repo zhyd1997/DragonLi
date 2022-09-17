b1f23587c74251232a6272d5a98e41fdb92e65ce
Yadong Zhang <zhyd007@gmail.com>
2022-09-17 10:07:56 +0800

    feat: rainbowkit integration.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,57 @@
 import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 import { globalStyles } from '@/styles/globals';
 import { Layout } from '@/components/Layout';
 import { Navbar } from '@/components/Navbar';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { chains, provider } = configureChains(
+    [
+      // mainnet
+      // chain.optimism,
+      // testnet
+      chain.goerli,
+    ],
+    [
+      alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+      publicProvider(),
+    ]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+  });
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  });
+
   return (
-    <Layout>
-      <Navbar />
-      {globalStyles}
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Layout>
+          <Navbar />
+          {globalStyles}
+          <Component {...pageProps} />
+        </Layout>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
