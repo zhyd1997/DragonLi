8242ccb7a77eabe4857359aae97da6b5b203f9a1
Yadong Zhang <zhyd007@gmail.com>
2022-09-18 17:26:32 +0800

    build: added 'export' script to support Fleek deployment. (#10)
    * fix: removed getServerSideProps.

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -3,6 +3,39 @@ import Head from 'next/head';
 import Typography from '@mui/material/Typography';
 import { Editor as Renderer } from '@/components/Editor';
 import type { Descendant } from 'slate';
+// fake content.
+const content: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: 'bold', bold: true },
+      {
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:',
+      },
+    ],
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+];
 
 interface Article {
   slug: string;
@@ -13,10 +46,16 @@ interface Article {
 };
 
 type ArticlePageProps = {
-  article: Article;
+  article?: Article;
 };
 
-const ArticlePage: NextPage<ArticlePageProps> = ({ article }) => {
+const ArticlePage: NextPage<ArticlePageProps> = ({ article = {
+  slug: 'example',
+  title: 'Example',
+  description: 'official example',
+  author: '0xe073B0fb1554390a47aBeCBDD42599a64c7D45DF',
+  content,
+} }) => {
   return (
     <div>
       <Head>
@@ -41,53 +80,5 @@ const ArticlePage: NextPage<ArticlePageProps> = ({ article }) => {
     </div>
   );
 };
-
-export const getServerSideProps = (): { props: ArticlePageProps } => {
-  // fake content.
-  const content: Descendant[] = [
-    {
-      type: 'paragraph',
-      children: [
-        { text: 'This is ' },
-        { text: 'rich', bold: true },
-        { text: ' text, ' },
-        { text: 'much', italic: true },
-        { text: ' better than a ' },
-        { text: '<textarea>', code: true },
-        { text: '!' },
-      ],
-    },
-    {
-      type: 'paragraph',
-      children: [
-        {
-          text:
-            "Since it's rich text, you can do things like turn a selection of text ",
-        },
-        { text: 'bold', bold: true },
-        {
-          text:
-            ', or add a semantically rendered block quote in the middle of the page, like this:',
-        },
-      ],
-    },
-    {
-      type: 'block-quote',
-      children: [{ text: 'A wise quote.' }],
-    },
-  ];
-
-  return {
-    props: {
-      article: {
-        slug: 'example',
-        title: 'Example',
-        description: 'official example',
-        author: '0xe073B0fb1554390a47aBeCBDD42599a64c7D45DF',
-        content,
-      }
-    },
-  }
-}
 
 export default ArticlePage;
